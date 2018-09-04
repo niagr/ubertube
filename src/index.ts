8385ff95c692e9985b2601ff64301736064d7fcc
Nishant <nishantagrwal12993@gmail.com>
2018-09-05 03:49:55 +0530

    Added CORS middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 require( 'app-module-path' ).addPath(__dirname);
+
 import express from 'express'
+import cors from 'cors'
 
 import mainRouter from './routes'
 
 const PORT = 8000
 
 const app = express()
+
+app.use(cors())
 
 app.use(mainRouter)
 
--- src/services/yt.ts
+++ src/services/yt.ts
@@ -1,16 +1,24 @@
+import url from 'url'
+
 import request from 'request-promise-native'
  
 export default class Youtube {
 
     private apiKey: string
 
+    private baseUrl = 'https://www.googleapis.com/youtube/v3'
+
     constructor (apiKey: string) {
         this.apiKey = apiKey
     }
 
-    async search (q: string) {
-        const data = await request.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=${this.apiKey}&q=${q}`)
+    private async get (apiUrl: string) {
+        const data = await request.get(this.baseUrl + apiUrl)
         return JSON.parse(data)
+    }
+
+    async search (q: string) {
+        return await this.get(`/search?part=snippet&key=${this.apiKey}&q=${q}`)
     }
 
 }
